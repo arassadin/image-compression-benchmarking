480e999d554d3dec4ebb89fed9c969fcb06ced28
Alexandr Rassadin <arassadin@mera.ru>
2016-10-08 19:50:06 +0300

    Added script for compression model downloading. Added one unified output format for test sampling generation

---
+++ download_model.sh
@@ -0,0 +1,8 @@
+echo "Downloading..."
+wget -q http://download.tensorflow.org/models/compression_residual_gru-2016-08-23.tar.gz -O /tmp/res_gru.tar.gz 
+echo "Extracting..."
+tar -xzf /tmp/res_gru.tar.gz -C /tmp/
+mkdir -p "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/google/model"
+mv -f /tmp/compression_residual_gru/* "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/google/model"
+rm -rf /tmp/res_gru.tar.gz /tmp/compression_residual_gru/
+echo "Done!"
